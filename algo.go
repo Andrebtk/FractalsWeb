8b1f032a683814fa4414e1f726dd87351476299c
Andre Bertok <53980377+Andrebtk@users.noreply.github.com>
2023-08-02 21:55:41 +0200

    Update algo.go

--- algo.go
+++ algo.go
@@ -4,6 +4,7 @@ package main
 import "math/rand"
 
 
+var f1, f2 func(float64, float64) (float64, float64)
 
 func fractal(u float64, typeFractal string) ([]float64 ,[]float64) {
 
@@ -13,7 +14,19 @@ func fractal(u float64, typeFractal string) ([]float64 ,[]float64) {
 	var x []float64 
 	var y []float64
 
-
+	if typeFractal == "custom1" {
+		f1 = func(x, y float64) (float64, float64) { return (x - y)/2 - 0.5, (x + y)/2 }
+		f2 = func(x, y float64) (float64, float64) { return (x + y)/2 + 0.5, (x - y)/2 + 0.5 }
+	} else if typeFractal == "custom2" {
+		f1 = func(x, y float64) (float64, float64) { return (x - y)/2, -(x + y)/2 }
+		f2 = func(x, y float64) (float64, float64) { return (x + y)/2 + 0.5, (x - y)/2 + 0.5 }
+	} else if typeFractal == "levyCurve" {
+		f1 = func(x, y float64) (float64, float64) { return (x - y)/2, (x + y)/2 }
+		f2 = func(x, y float64) (float64, float64) { return (x + y)/2 + 0.5, -(x - y)/2 + 0.5 }
+	} else if typeFractal == "dragonCurve" {
+		f1 = func(x, y float64) (float64, float64) { return (x - y)/2, (x + y)/2 }
+		f2 = func(x, y float64) (float64, float64) { return -(x + y)/2 + 0.5, (x - y)/2 + 0.5 }
+	}
 
 	x = append(x, 0)
 	y = append(y, 0)
@@ -22,38 +35,10 @@ func fractal(u float64, typeFractal string) ([]float64 ,[]float64) {
 		var randNum int = rand.Intn(2) + 1
 		
 		
-		if typeFractal == "custom1"{	
-
-			if  randNum==1  {
-				x[i], y[i] = customF1(x[i], y[i])
-			}else{
-				x[i], y[i] = customF2(x[i], y[i])
-			}
-
-		}else if typeFractal == "custom2"{
-
-			if  randNum==1  {
-				x[i], y[i] = custom2F1(x[i], y[i])
-			}else{
-				x[i], y[i] = custom2F2(x[i], y[i])
-			}
-
-		}else if typeFractal == "levyCurve"{
-
-			if  randNum==1  {
-				x[i], y[i] = levyCurveF1(x[i], y[i])
-			}else{
-				x[i], y[i] = levyCurveF2(x[i], y[i])
-			}
-
-		}else if typeFractal == "dragonCurve"{
-
-			if  randNum==1  {
-				x[i], y[i] = dragonCurveF1(x[i], y[i])
-			}else{
-				x[i], y[i] = dragonCurveF2(x[i], y[i])
-			}
-
+		if  randNum==1  {
+			x[i], y[i] = f1(x[i], y[i])
+		}else{
+			x[i], y[i] = f2(x[i], y[i])
 		}
 		
 
@@ -63,50 +48,3 @@ func fractal(u float64, typeFractal string) ([]float64 ,[]float64) {
 
 	return x,y
 }
-
-
-
-
-
-func customF1(x float64, y float64) (float64, float64) {
-	return (x-y)/2 - 0.5, (x+y)/2 
-}
-
-func customF2(x, y float64) (float64, float64) {
-	return (x+y)/2 + 0.5, (x-y)/2 + 0.5 
-
-}
-
-
-
-func custom2F1(x float64, y float64) (float64, float64) {
-	return (x-y)/2, -(x+y)/2 
-}
-
-func custom2F2(x, y float64) (float64, float64) {
-	return (x+y)/2 + 0.5, (x-y)/2 + 0.5 
-
-}
-
-
-
-func levyCurveF1(x float64, y float64) (float64, float64) {
-	return (x-y)/2, (x+y)/2 
-}
-
-func levyCurveF2(x, y float64) (float64, float64) {
-	return (x+y)/2 + 0.5, -(x-y)/2 + 0.5 
-
-}
-
-
-
-func dragonCurveF1(x float64, y float64) (float64, float64) {
-	return (x-y)/2, (x+y)/2 
-}
-
-func dragonCurveF2(x, y float64) (float64, float64) {
-	return -(x+y)/2 + 0.5, (x-y)/2 + 0.5 
-
-}
-
